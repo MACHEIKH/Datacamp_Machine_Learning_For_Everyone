528d5754f423467d7efcc2177c362232c593669f
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:40:44 +0600

    Update 6_ImportPackage.py

--- 1_Introduction_to_Python/3_Functions_and_Packages/6_ImportPackage.py
+++ 1_Introduction_to_Python/3_Functions_and_Packages/6_ImportPackage.py
@@ -1,17 +1,17 @@
-Import package
-As a data scientist, some notions of geometry never hurt. Let's refresh some of the basics.
+# Import package
+# As a data scientist, some notions of geometry never hurt. Let's refresh some of the basics.
 
-For a fancy clustering algorithm, you want to find the circumference, C, and area, A, of a circle. When the radius of the circle is r, you can calculate C and A as:
+# For a fancy clustering algorithm, you want to find the circumference, C, and area, A, of a circle. When the radius of the circle is r, you can calculate C and A as:
 
-C=2πr
-A=πr2
-To use the constant pi, you'll need the math package. A variable r is already coded in the script. Fill in the code to calculate C and A and see how the print() functions create some nice printouts.
+# C=2πr
+# A=πr2
+# To use the constant pi, you'll need the math package. A variable r is already coded in the script. Fill in the code to calculate C and A and see how the print() functions create some nice printouts.
 
-Instructions
-100 XP
-Import the math package. Now you can access the constant pi with math.pi.
-Calculate the circumference of the circle and store it in C.
-Calculate the area of the circle and store it in A.
+# Instructions
+# 100 XP
+# Import the math package. Now you can access the constant pi with math.pi.
+# Calculate the circumference of the circle and store it in C.
+# Calculate the area of the circle and store it in A.
 
 # Definition of radius
 r = 0.43
