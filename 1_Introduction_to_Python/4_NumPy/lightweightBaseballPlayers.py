aa68004501a33d14285c3076b60b638eca6ee1f2
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:53:29 +0600

    Update lightweightBaseballPlayers.py

--- 1_Introduction_to_Python/4_NumPy/lightweightBaseballPlayers.py
+++ 1_Introduction_to_Python/4_NumPy/lightweightBaseballPlayers.py
@@ -1,22 +1,22 @@
-Lightweight baseball players
-To subset both regular Python lists and numpy arrays, you can use square brackets:
+# Lightweight baseball players
+# To subset both regular Python lists and numpy arrays, you can use square brackets:
 
-x = [4 , 9 , 6, 3, 1]
-x[1]
-import numpy as np
-y = np.array(x)
-y[1]
-For numpy specifically, you can also use boolean numpy arrays:
+# x = [4 , 9 , 6, 3, 1]
+# x[1]
+# import numpy as np
+# y = np.array(x)
+# y[1]
+# For numpy specifically, you can also use boolean numpy arrays:
 
-high = y > 5
-y[high]
-The code that calculates the BMI of all baseball players is already included. Follow the instructions and reveal interesting things from the data!
+# high = y > 5
+# y[high]
+# The code that calculates the BMI of all baseball players is already included. Follow the instructions and reveal interesting things from the data!
 
-Instructions
-100 XP
-Create a boolean numpy array: the element of the array should be True if the corresponding baseball player's BMI is below 21. You can use the < operator for this. Name the array light.
-Print the array light.
-Print out a numpy array with the BMIs of all baseball players whose BMI is below 21. Use light inside square brackets to do a selection on the bmi array.
+# Instructions
+# 100 XP
+# Create a boolean numpy array: the element of the array should be True if the corresponding baseball player's BMI is below 21. You can use the < operator for this. Name the array light.
+# Print the array light.
+# Print out a numpy array with the BMIs of all baseball players whose BMI is below 21. Use light inside square brackets to do a selection on the bmi array.
 
 # height and weight are available as a regular lists
 
