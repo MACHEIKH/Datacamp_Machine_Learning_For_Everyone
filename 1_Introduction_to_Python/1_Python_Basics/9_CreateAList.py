44c4c1bb6c0cd0fefb7681be3551870f2d6c6e77
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:21:48 +0600

    Update 9_CreateAList.py

--- 1_Introduction_to_Python/1_Python_Basics/9_CreateAList.py
+++ 1_Introduction_to_Python/1_Python_Basics/9_CreateAList.py
@@ -1,15 +1,15 @@
-Create a list
-As opposed to int, bool etc., a list is a compound data type; you can group values together:
+# Create a list
+# As opposed to int, bool etc., a list is a compound data type; you can group values together:
 
-a = "is"
-b = "nice"
-my_list = ["my", "list", a, b]
-After measuring the height of your family, you decide to collect some information on the house you're living in. The areas of the different parts of your house are stored in separate variables for now, as shown in the script.
+# a = "is"
+# b = "nice"
+# my_list = ["my", "list", a, b]
+# After measuring the height of your family, you decide to collect some information on the house you're living in. The areas of the different parts of your house are stored in separate variables for now, as shown in the script.
 
-Instructions
-100 XP
-Create a list, areas, that contains the area of the hallway (hall), kitchen (kit), living room (liv), bedroom (bed) and bathroom (bath), in this order. Use the predefined variables.
-Print areas with the print() function.
+# Instructions
+# 100 XP
+# Create a list, areas, that contains the area of the hallway (hall), kitchen (kit), living room (liv), bedroom (bed) and bathroom (bath), in this order. Use the predefined variables.
+# Print areas with the print() function.
 
 # area variables (in square meters)
 hall = 11.25
