8c75e7b490b8b6a652459e85eec3314604c85b6d
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:50:43 +0600

    Update yourFirstNumPyArray.py

--- 1_Introduction_to_Python/4_NumPy/yourFirstNumPyArray.py
+++ 1_Introduction_to_Python/4_NumPy/yourFirstNumPyArray.py
@@ -1,13 +1,13 @@
-Your First NumPy Array
-In this chapter, we're going to dive into the world of baseball. Along the way, you'll get comfortable with the basics of numpy, a powerful package to do data science.
+# Your First NumPy Array
+# In this chapter, we're going to dive into the world of baseball. Along the way, you'll get comfortable with the basics of numpy, a powerful package to do data science.
 
-A list baseball has already been defined in the Python script, representing the height of some baseball players in centimeters. Can you add some code here and there to create a numpy array from it?
+# A list baseball has already been defined in the Python script, representing the height of some baseball players in centimeters. Can you add some code here and there to create a numpy array from it?
 
-Instructions
-100 XP
-Import the numpy package as np, so that you can refer to numpy with np.
-Use np.array() to create a numpy array from baseball. Name this array np_baseball.
-Print out the type of np_baseball to check that you got it right.
+# Instructions
+# 100 XP
+# Import the numpy package as np, so that you can refer to numpy with np.
+# Use np.array() to create a numpy array from baseball. Name this array np_baseball.
+# Print out the type of np_baseball to check that you got it right.
 
 # Create list baseball
 baseball = [180, 215, 210, 210, 188, 176, 209, 200]
