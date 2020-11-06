5ce609fc865154c41c57a9593cbb65aa25452077
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:29:12 +0600

    Update 6_SlicingAndDicing.py

--- 1_Introduction_to_Python/2_Python_Lists/6_SlicingAndDicing.py
+++ 1_Introduction_to_Python/2_Python_Lists/6_SlicingAndDicing.py
@@ -1,20 +1,20 @@
-Slicing and dicing
-Selecting single values from a list is just one part of the story. It's also possible to slice your list, which means selecting multiple elements from your list. Use the following syntax:
+# Slicing and dicing
+# Selecting single values from a list is just one part of the story. It's also possible to slice your list, which means selecting multiple elements from your list. Use the following syntax:
 
-my_list[start:end]
-The start index will be included, while the end index is not.
+# my_list[start:end]
+# The start index will be included, while the end index is not.
 
-The code sample below shows an example. A list with "b" and "c", corresponding to indexes 1 and 2, are selected from a list x:
+# The code sample below shows an example. A list with "b" and "c", corresponding to indexes 1 and 2, are selected from a list x:
 
-x = ["a", "b", "c", "d"]
-x[1:3]
-The elements with index 1 and 2 are included, while the element with index 3 is not.
+# x = ["a", "b", "c", "d"]
+# x[1:3]
+# The elements with index 1 and 2 are included, while the element with index 3 is not.
 
-Instructions
-100 XP
-Use slicing to create a list, downstairs, that contains the first 6 elements of areas.
-Do a similar thing to create a new variable, upstairs, that contains the last 4 elements of areas.
-Print both downstairs and upstairs using print().
+# Instructions
+# 100 XP
+# Use slicing to create a list, downstairs, that contains the first 6 elements of areas.
+# Do a similar thing to create a new variable, upstairs, that contains the last 4 elements of areas.
+# Print both downstairs and upstairs using print().
 
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
