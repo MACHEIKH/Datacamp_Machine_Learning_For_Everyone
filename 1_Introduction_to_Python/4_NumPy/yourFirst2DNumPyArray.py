9bf6a33e958dbab4d70782f58627508cbf116378
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:55:00 +0600

    Update yourFirst2DNumPyArray.py

--- 1_Introduction_to_Python/4_NumPy/yourFirst2DNumPyArray.py
+++ 1_Introduction_to_Python/4_NumPy/yourFirst2DNumPyArray.py
@@ -1,13 +1,13 @@
-Your First 2D NumPy Array
-Before working on the actual MLB data, let's try to create a 2D numpy array from a small list of lists.
+# Your First 2D NumPy Array
+# Before working on the actual MLB data, let's try to create a 2D numpy array from a small list of lists.
 
-In this exercise, baseball is a list of lists. The main list contains 4 elements. Each of these elements is a list containing the height and the weight of 4 baseball players, in this order. baseball is already coded for you in the script.
+# In this exercise, baseball is a list of lists. The main list contains 4 elements. Each of these elements is a list containing the height and the weight of 4 baseball players, in this order. baseball is already coded for you in the script.
 
-Instructions
-100 XP
-Use np.array() to create a 2D numpy array from baseball. Name it np_baseball.
-Print out the type of np_baseball.
-Print out the shape attribute of np_baseball. Use np_baseball.shape.
+# Instructions
+# 100 XP
+# Use np.array() to create a 2D numpy array from baseball. Name it np_baseball.
+# Print out the type of np_baseball.
+# Print out the shape attribute of np_baseball. Use np_baseball.shape.
 
 # Create baseball, a list of lists
 baseball = [[180, 78.4],
