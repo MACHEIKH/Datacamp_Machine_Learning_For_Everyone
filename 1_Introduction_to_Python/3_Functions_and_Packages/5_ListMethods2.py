62a8506a41be88bbd5f535a674bd69689245735c
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-06 20:37:50 +0600

    Update 5_ListMethods2.py

--- 1_Introduction_to_Python/3_Functions_and_Packages/5_ListMethods2.py
+++ 1_Introduction_to_Python/3_Functions_and_Packages/5_ListMethods2.py
@@ -1,17 +1,17 @@
-List Methods (2)
-Most list methods will change the list they're called on. Examples are:
+# List Methods (2)
+# Most list methods will change the list they're called on. Examples are:
 
-append(), that adds an element to the list it is called on,
-remove(), that removes the first element of a list that matches the input, and
-reverse(), that reverses the order of the elements in the list it is called on.
-You'll be working on the list with the area of different parts of the house: areas.
+# append(), that adds an element to the list it is called on,
+# remove(), that removes the first element of a list that matches the input, and
+# reverse(), that reverses the order of the elements in the list it is called on.
+# You'll be working on the list with the area of different parts of the house: areas.
 
-Instructions
-100 XP
-Use append() twice to add the size of the poolhouse and the garage again: 24.5 and 15.45, respectively. Make sure to add them in this order.
-Print out areas
-Use the reverse() method to reverse the order of the elements in areas.
-Print out areas once more.
+# Instructions
+# 100 XP
+# Use append() twice to add the size of the poolhouse and the garage again: 24.5 and 15.45, respectively. Make sure to add them in this order.
+# Print out areas
+# Use the reverse() method to reverse the order of the elements in areas.
+# Print out areas once more.
 
 # Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
