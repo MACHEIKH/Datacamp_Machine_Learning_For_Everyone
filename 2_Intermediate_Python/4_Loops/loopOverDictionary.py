9564c856430afae46c3954ce12074e0aef445174
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-07 13:31:45 +0600

    Update loopOverDictionary.py

--- 2_Intermediate_Python/4_Loops/loopOverDictionary.py
+++ 2_Intermediate_Python/4_Loops/loopOverDictionary.py
@@ -1,17 +1,17 @@
-Loop over dictionary
-In Python 3, you need the items() method to loop over a dictionary:
+# Loop over dictionary
+# In Python 3, you need the items() method to loop over a dictionary:
 
-world = { "afghanistan":30.55, 
-          "albania":2.77,
-          "algeria":39.21 }
+# world = { "afghanistan":30.55, 
+#           "albania":2.77,
+#           "algeria":39.21 }
 
-for key, value in world.items() :
-    print(key + " -- " + str(value))
-Remember the europe dictionary that contained the names of some European countries as key and their capitals as corresponding value? Go ahead and write a loop to iterate over it!
+# for key, value in world.items() :
+#     print(key + " -- " + str(value))
+# Remember the europe dictionary that contained the names of some European countries as key and their capitals as corresponding value? Go ahead and write a loop to iterate over it!
 
-Instructions
-100 XP
-Write a for loop that goes through each key:value pair of europe. On each iteration, "the capital of x is y" should be printed out, where x is the key and y is the value of the pair.
+# Instructions
+# 100 XP
+# Write a for loop that goes through each key:value pair of europe. On each iteration, "the capital of x is y" should be printed out, where x is the key and y is the value of the pair.
 
 
 # Definition of dictionary
