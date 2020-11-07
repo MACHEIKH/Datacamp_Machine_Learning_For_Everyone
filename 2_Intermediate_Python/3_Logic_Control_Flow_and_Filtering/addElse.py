b758603cae214b86c957cf6e0c41e21eb00894fa
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-07 13:23:07 +0600

    Update addElse.py

--- 2_Intermediate_Python/3_Logic_Control_Flow_and_Filtering/addElse.py
+++ 2_Intermediate_Python/3_Logic_Control_Flow_and_Filtering/addElse.py
@@ -1,11 +1,11 @@
-Add else
-On the right, the if construct for room has been extended with an else statement so that "looking around elsewhere." is printed if the condition room == "kit" evaluates to False.
+# Add else
+# On the right, the if construct for room has been extended with an else statement so that "looking around elsewhere." is printed if the condition room == "kit" evaluates to False.
 
-Can you do a similar thing to add more functionality to the if construct for area?
+# Can you do a similar thing to add more functionality to the if construct for area?
 
-Instructions
-100 XP
-Add an else statement to the second control structure so that "pretty small." is printed out if area > 15 evaluates to False.
+# Instructions
+# 100 XP
+# Add an else statement to the second control structure so that "pretty small." is printed out if area > 15 evaluates to False.
 
 
 # Define variables
