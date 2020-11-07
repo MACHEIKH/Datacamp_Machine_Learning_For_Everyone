b32e7016d36c1ce0590bed20a1bd890794abc56f
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-07 13:17:22 +0600

    Update locAndiloc3.py

--- 2_Intermediate_Python/2_Dictionaries_&_Pandas/locAndiloc3.py
+++ 2_Intermediate_Python/2_Dictionaries_&_Pandas/locAndiloc3.py
@@ -1,16 +1,16 @@
-loc and iloc (3)
-It's also possible to select only columns with loc and iloc. In both cases, you simply put a slice going from beginning to end in front of the comma:
+# loc and iloc (3)
+# It's also possible to select only columns with loc and iloc. In both cases, you simply put a slice going from beginning to end in front of the comma:
 
-cars.loc[:, 'country']
-cars.iloc[:, 1]
+# cars.loc[:, 'country']
+# cars.iloc[:, 1]
 
-cars.loc[:, ['country','drives_right']]
-cars.iloc[:, [1, 2]]
-Instructions
-100 XP
-Print out the drives_right column as a Series using loc or iloc.
-Print out the drives_right column as a DataFrame using loc or iloc.
-Print out both the cars_per_cap and drives_right column as a DataFrame using loc or iloc.
+# cars.loc[:, ['country','drives_right']]
+# cars.iloc[:, [1, 2]]
+# Instructions
+# 100 XP
+# Print out the drives_right column as a Series using loc or iloc.
+# Print out the drives_right column as a DataFrame using loc or iloc.
+# Print out both the cars_per_cap and drives_right column as a DataFrame using loc or iloc.
 
 
 # Import cars data
