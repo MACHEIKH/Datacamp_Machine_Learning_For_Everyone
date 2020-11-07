e351e931395f42b4057726f3aef6fb1d605fc853
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-07 13:30:19 +0600

    Update indexesAndValues2.py

--- 2_Intermediate_Python/4_Loops/indexesAndValues2.py
+++ 2_Intermediate_Python/4_Loops/indexesAndValues2.py
@@ -1,9 +1,9 @@
-Indexes and values (2)
-For non-programmer folks, room 0: 11.25 is strange. Wouldn't it be better if the count started at 1?
+# Indexes and values (2)
+# For non-programmer folks, room 0: 11.25 is strange. Wouldn't it be better if the count started at 1?
 
-Instructions
-100 XP
-Adapt the print() function in the for loop on the right so that the first printout becomes "room 1: 11.25", the second one "room 2: 18.0" and so on.
+# Instructions
+# 100 XP
+# Adapt the print() function in the for loop on the right so that the first printout becomes "room 1: 11.25", the second one "room 2: 18.0" and so on.
 
 
 # areas list
