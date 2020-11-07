14979a0ae96d49706fe622a0207198e69b64b26c
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-07 13:02:38 +0600

    Update additionalCustomizations.py

--- 2_Intermediate_Python/1_Matplotlib/additionalCustomizations.py
+++ 2_Intermediate_Python/1_Matplotlib/additionalCustomizations.py
@@ -1,9 +1,9 @@
-Additional Customizations
-If you have another look at the script, under # Additional Customizations, you'll see that there are two plt.text() functions now. They add the words "India" and "China" in the plot.
+# Additional Customizations
+# If you have another look at the script, under # Additional Customizations, you'll see that there are two plt.text() functions now. They add the words "India" and "China" in the plot.
 
-Instructions
-100 XP
-Add plt.grid(True) after the plt.text() calls so that gridlines are drawn on the plot.
+# Instructions
+# 100 XP
+# Add plt.grid(True) after the plt.text() calls so that gridlines are drawn on the plot.
 
 
 # Scatter plot
