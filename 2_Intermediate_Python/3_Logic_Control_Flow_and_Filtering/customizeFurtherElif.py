3301f64d1919f0f8dfba733c8f6918a08bf54f92
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-07 13:23:46 +0600

    Update customizeFurtherElif.py

--- 2_Intermediate_Python/3_Logic_Control_Flow_and_Filtering/customizeFurtherElif.py
+++ 2_Intermediate_Python/3_Logic_Control_Flow_and_Filtering/customizeFurtherElif.py
@@ -1,11 +1,11 @@
-Customize further: elif
-It's also possible to have a look around in the bedroom. The sample code contains an elif part that checks if room equals "bed". In that case, "looking around in the bedroom." is printed out.
+# Customize further: elif
+# It's also possible to have a look around in the bedroom. The sample code contains an elif part that checks if room equals "bed". In that case, "looking around in the bedroom." is printed out.
 
-It's up to you now! Make a similar addition to the second control structure to further customize the messages for different values of area.
+# It's up to you now! Make a similar addition to the second control structure to further customize the messages for different values of area.
 
-Instructions
-100 XP
-Add an elif to the second control structure such that "medium size, nice!" is printed out if area is greater than 10.
+# Instructions
+# 100 XP
+# Add an elif to the second control structure such that "medium size, nice!" is printed out if area is greater than 10.
 
 
 # Define variables
