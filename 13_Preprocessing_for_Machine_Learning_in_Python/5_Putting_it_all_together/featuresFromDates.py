5a718a453cf68b565ddf36e1e87eb30262dc0281
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-16 18:05:26 +0600

    Create featuresFromDates.py

---
+++ 13_Preprocessing_for_Machine_Learning_in_Python/5_Putting_it_all_together/featuresFromDates.py
@@ -0,0 +1,22 @@
+# Features from dates
+# Another feature engineering task to perform is month and year extraction. Perform this task on the date column of the ufo dataset.
+
+# Instructions
+# 100 XP
+# Print out the head() of the date column.
+# Using apply(), lambda, and the .month attribute, extract the month from the date column.
+# Using apply(), lambda, and the .year attribute, extract the year from the date column.
+# Take a look at the head() of the date, month, and year columns.
+
+
+# Look at the first 5 rows of the date column
+print(ufo['date'].head())
+
+# Extract the month from the date column
+ufo["month"] = ufo["date"].apply(lambda x: x.month)
+
+# Extract the year from the date column
+ufo["year"] = ufo["date"].apply(lambda x: x.year)
+
+# Take a look at the head of all three columns
+print(ufo[['date', 'month', 'year']].head())
