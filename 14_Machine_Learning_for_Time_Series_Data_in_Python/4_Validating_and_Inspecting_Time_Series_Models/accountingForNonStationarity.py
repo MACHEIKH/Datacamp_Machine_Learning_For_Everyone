fbeaa78853c5add3410e362ec8c1278b1c2d90bc
thequietone18-glitch <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-17 09:24:08 +0600

    Create accountingForNonStationarity.py

---
+++ 14_Machine_Learning_for_Time_Series_Data_in_Python/4_Validating_and_Inspecting_Time_Series_Models/accountingForNonStationarity.py
@@ -0,0 +1,36 @@
+# Accounting for non-stationarity
+# In this exercise, you will again visualize the variations in model scores, but now for data that changes its statistics over time.
+
+# An instance of the Linear regression model object is stored in model, a cross-validation object in cv, and the data in X and y.
+
+# Instructions 1/2
+# 90 XP
+# Create an empty DataFrame to collect the results.
+# Iterate through multiple window sizes, each time creating a new TimeSeriesSplit object.
+# Calculate the cross-validated scores (using a custom scorer we defined for you, my_pearsonr) of the model on training data.
+
+# Instructions 2/2
+# 10 XP
+# Run the given code to plot the results.
+
+
+# # Pre-initialize window sizes (Instruction 1)
+# window_sizes = [25, 50, 75, 100]
+
+# # Create an empty DataFrame to collect the stores
+# all_scores = pd.DataFrame(index=times_scores)
+
+# # Generate scores for each split to see how the model performs over time
+# for window in window_sizes:
+#     # Create cross-validation object using a limited lookback window
+#     cv = TimeSeriesSplit(n_splits=100, max_train_size=window)
+    
+#     # Calculate scores across all CV splits and collect them in a DataFrame
+#     this_scores = cross_val_score(model, X, y, cv=cv, scoring=my_pearsonr)
+#     all_scores['Length {}'.format(window)] = this_scores
+
+
+# Visualize the scores (Instruction 2)
+ax = all_scores.rolling(10).mean().plot(cmap=plt.cm.coolwarm)
+ax.set(title='Scores for multiple windows', ylabel='Correlation (r)')
+plt.show()
