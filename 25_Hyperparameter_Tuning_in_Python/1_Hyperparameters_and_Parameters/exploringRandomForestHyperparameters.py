cd204642c8677c579438a51a78945427ba9cabe7
Ehsanul Hoque <70466614+thequietone18-glitch@users.noreply.github.com>
2020-11-23 17:36:28 +0600

    Create exploringRandomForestHyperparameters.py

---
+++ 25_Hyperparameter_Tuning_in_Python/1_Hyperparameters_and_Parameters/exploringRandomForestHyperparameters.py
@@ -0,0 +1,40 @@
+# Exploring Random Forest Hyperparameters
+# Understanding what hyperparameters are available and the impact of different hyperparameters is a core skill for any data scientist. As models become more complex, there are many different settings you can set, but only some will have a large impact on your model.
+
+# You will now assess an existing random forest model (it has some bad choices for hyperparameters!) and then make better choices for a new random forest model and assess its performance.
+
+# You will have available:
+
+# X_train, X_test, y_train, y_test DataFrames
+# An existing pre-trained random forest estimator, rf_clf_old
+# The predictions of the existing random forest estimator on the test set, rf_old_predictions
+# Instructions 1/3
+# 35 XP
+# Print out the hyperparameters of the existing random forest classifier by printing the estimator and then create a confusion matrix and accuracy score from it. The test set y_test and the old predictions rf_old_predictions will be quite useful!
+
+# Instructions 2/3
+# 35 XP
+# Create a new random forest classifier with a better n_estimators (try 500) then fit this to the data and obtain predictions.
+
+# Instructions 3/3
+# 30 XP
+# Assess the performance of the new random forest classifier. Create the confusion matrix and accuracy score and print them out. How does this compare to the first model you were given?
+
+
+# Print out the old estimator, notice which hyperparameter is badly set (Instruction 1)
+print(rf_clf_old)
+
+# Get confusion matrix & accuracy for the old rf_model
+print("Confusion Matrix: \n\n {} \n Accuracy Score: \n\n {}".format(
+  confusion_matrix(y_test, rf_old_predictions),
+  accuracy_score(y_test, rf_old_predictions))) 
+
+# Create a new random forest classifier with better hyperparamaters (Instruction 2)
+rf_clf_new = RandomForestClassifier(n_estimators=500)
+
+# Fit this to the data and obtain predictions
+rf_new_predictions = rf_clf_new.fit(X_train, y_train).predict(X_test)
+
+# Assess the new model (using new predictions!) (Instruction 3)
+print("Confusion Matrix: \n\n", confusion_matrix(y_test, rf_new_predictions))
+print("Accuracy Score: \n\n", accuracy_score(y_test, rf_new_predictions))
